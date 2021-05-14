efd8677d305f21143979003cfef63a261fa20a81
csevil-coder <59279385+csevil-coder@users.noreply.github.com>
2021-05-14 18:23:42 +0300

    change txt

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", function(req, res){
-    res.send("WORKING!!!")
+    res.send("<h1> NICOLETA IS THE BEST FUCK OF THE REST!!!</h1>")
 });
 
 app.listen(process.env.PORT || 5000 );
